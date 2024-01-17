acdd00a24dbd8d4ebcf6d39fc1df45a82b2d1bfc
vallduuu <73582485+vallduuu@users.noreply.github.com>
2024-01-17 11:18:04 +0100

    Update contador3.php

--- contador3.php
+++ contador3.php
@@ -1,14 +1,15 @@
 <?php
 
-$contador=0;
-if($fd = fopen('counter.txt','r')){
-   $contador=fgets($fd);
-   fclose($fd);
+$contador = 0;
+if ($fd = fopen('counter.txt', 'r')) {
+    $contador = fgets($fd);
+    fclose($fd);
 }
 
-$fd2=fopen('counter.txt','w');
-$contador=$contador+1;
-fwrite($fd2,$contador);
+$fd2 = fopen('counter.txt', 'w');
+$contador = intval($contador) + 1;  // Convertir a entero antes de sumar 1
+fwrite($fd2, $contador);
 fclose($fd2);
 
 echo $contador;
+?>
