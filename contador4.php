0aa2a1c484e245a763842b2427f2d2605843f158
vallduuu <73582485+vallduuu@users.noreply.github.com>
2024-01-17 11:19:58 +0100

    Update contador4.php

--- contador4.php
+++ contador4.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
 $servername = "localhost";
 $username = "admin";
 $password = "12345";
@@ -11,17 +10,18 @@ $dbname = "estadistics";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-if($conn -> connect_error){
-   echo "Error en connectar a MySQL: " . $CONN -> connect_error;
-   exit();
+if ($conn->connect_error) {
+   die("Error en conectar a MySQL: " . $conn->connect_error);
 }
 
-$ip=$_SERVER['REMOTE_ADDR'];
-$sql="INSERT INTO registre(ip) VALUES ('" . $ip . "')";
-$conn ->query($sql);
+$ip = $_SERVER['REMOTE_ADDR'];
+$sql = "INSERT INTO registre(ip) VALUES ('" . $ip . "')";
+$conn->query($sql);
 
 $resultat = $conn->query("SELECT COUNT(*) FROM registre");
 $row = mysqli_fetch_array($resultat);
 echo $row[0];
 
 $conn->close();
+?>
+
